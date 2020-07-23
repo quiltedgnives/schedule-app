7b8909ed07ab578a07ca80e5ae8837eb221b6e8c
Henry Doan <henrydoan96@gmail.com>
2020-07-23 15:35:17 -0600

    added contact details

--- schedule-client/src/providers/ContactProvider.js
+++ schedule-client/src/providers/ContactProvider.js
@@ -6,10 +6,10 @@ export const ContactConsumer = ContactContext.Consumer;
 
 class ContactProvider extends Component {
   state = { contact: {
-    address: '',
-    email: '',
-    phone: '', 
-    map: '',
+    address: '123 street',
+    email: 'apples@apple.com',
+    phone: '123-123-1234', 
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d768741.4841754775!2d-113.04034005926903!3d41.18042629164514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875335b1dff77dc7%3A0xe4f87e4c6ef796fb!2sGreat%20Salt%20Lake!5e0!3m2!1sen!2sus!4v1595539908724!5m2!1sen!2sus",
   }, contacts: [] }
 
   handleHelp = (help, history) => {
