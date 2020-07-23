6cad494b8ffc3ee4ce836c67cff9db221d96c293
Dax Allen <dax.e.allen@gmail.com>
2020-07-23 10:16:09 -0600

    Unknown local changes that would be overwritten

--- schedule-backend/src/database/models.py
+++ schedule-backend/src/database/models.py
@@ -1,15 +1,14 @@
-
 from .db import db
 
 class Business(db.Document):
-bus_name = db.StringField(required=True)
-workers_name = db.StringField(required=True)
-location = db.StringField(required=True)
-admin = db.StringField(required=True)
-password = db.StringField(required=True)
+    bus_name = db.StringField(required=True)
+    workers_name = db.StringField(required=True)
+    location = db.StringField(required=True)
+    admin = db.StringField(required=True)
+    password = db.StringField(required=True)
     
 class Client(db.Document):
-client_name = db.StringField(required=True)
-dob = db.StringField(required=True)
-pref_worker = db.StringField(required=False)
-customer_id = db.StringField(required=True)
+    client_name = db.StringField(required=True)
+    dob = db.StringField(required=True)
+    pref_worker = db.StringField(required=False)
+    customer_id = db.StringField(required=True)
